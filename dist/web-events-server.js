6af43fc3a9542c5f10161adc1b2fc2a78bfe2b04
Dmitry Lozhkin <31573763+Coder911th@users.noreply.github.com>
2018-02-23 00:13:35 +0300

    1.1.0 update

--- dist/web-events-server.js
+++ dist/web-events-server.js
@@ -7,8 +7,51 @@ function events(server, evs) {
         // Создаём WebSocket-сервер
         wss = new ws.Server({ server }),
 
-        // Объект с открытыми подключениями вида { uid: socket }
-        connections = {};
+        // Объект с открытыми подключениями вида { uid: { socket, data } }
+        connections = {
+
+            /*** Methods for work with data of each connection ****/
+
+            // Simulates forEach method of arrays
+            forEach(callback) {
+                for (let key in connections) {
+                    if (connections[key] instanceof Function == false)
+                        if (!connections[key].data.irnore)
+                            callback(connections[key].data);}
+            },
+        
+            // Simulates map method of arrays
+            map(callback) {
+                let result = [];
+                for (let key in connections)
+                    if (connections[key] instanceof Function == false)
+                        if (!connections[key].data.irnore)
+                            result.push(callback(connections[key].data));
+                return result;
+            }
+        },
+        
+        connectionsProxy = new Proxy(connections, {
+            get(target, prop) {
+                
+                // Access to the special methods
+                if (target[prop] instanceof Function)
+                    return target[prop];
+
+                /*
+                    Providing a direct access to the data object
+                    of every client through proxy
+                */
+                if (target[prop] instanceof Object)
+                    return target[prop].data;
+                    
+                return target[prop];
+            },
+
+            set(target, prop, value) {
+                return false; // Preventing the setting of value
+            }
+        });
 
     /* 
         Функция, возвращающая уникальный ключ 
@@ -82,7 +125,7 @@ function events(server, evs) {
         if (args.length == 1 && typeof args[0] == 'object')
             args = args[0]; // Если аргументы были переданы через объект (массив)
 
-        connections[this.uid].send(JSON.stringify({
+        connections[this.uid].socket.send(JSON.stringify({
             type: eventName,
             args: args
         }));
@@ -93,7 +136,7 @@ function events(server, evs) {
         Вызывается в контексте объета клиента
     */
     function close() {
-        connections[this.uid].close();
+        connections[this.uid].socket.close();
     }
 
     /* 
@@ -112,13 +155,19 @@ function events(server, evs) {
 
         // Выдаём новому подключившемуся клиенту уникальный ключ
         let client = {
+            // Флаг игнорирования текущего клиента для перебирающих методов connections
+            irnore: true,
             uid: getUniqueKey(connections),
             emit: emit,
-            close: close
+            close: close,
+            base: connectionsProxy  // База активных подключений
         };
 
         // Сохраняем сокет активного клиента под его uid
-        connections[client.uid] = ws;
+        connections[client.uid] = {
+            socket: ws,
+            data: client
+        };
 
         /*
             После установки соединения с новым клиентом
@@ -127,6 +176,9 @@ function events(server, evs) {
         */
         if (evs.connection)
             returnEmit(evs.connection, client, ws, req);
+
+        // Больше клиента не игнорируем в методах перебора
+        client.irnore = false;
 
         ws.on('message', data => {
             // Предполагается, что данные приходят в JSON-формате
@@ -150,9 +202,12 @@ function events(server, evs) {
 
         });
 
-        ws.on('error', function() {console.log('error!')}); // Заглушка
+        ws.on('error', function() {}); // Заглушка
 
-        ws.on('close', (code, reason) => {console.log('close!');
+        ws.on('close', (code, reason) => {
+            // Removing connection from base
+            delete connections[client.uid];
+
             /*
                 Вызываем обработчик закрытия соединения, 
                 если таковой имеется
@@ -162,7 +217,9 @@ function events(server, evs) {
         });
 
     });
-        
+
+    // Proxy return for managing active connections
+    return connectionsProxy;
 }
 
 module.exports = events;
